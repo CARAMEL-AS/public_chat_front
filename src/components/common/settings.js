65c44cbe5a2a160afb8686cc15e923323139990b
Aftab Sidhu <Aftab180@gmail.com>
2022-07-13 19:16:15 -0400

    update

--- src/components/common/settings.js
+++ src/components/common/settings.js
@@ -26,7 +26,15 @@ const Settings = (props) => {
                 <input value={userNameInput} onChange={(e) => setUserNameInput(e.target.value)} style={{width: '70%', height: '30%', borderRadius: 8, textAlign: 'center', fontWeight: 'bold', marginTop: '-2%'}} />
             </div>
             <div onClick={logout} display={user?.username !== userNameInput} style={{width: '86%', height: '10%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: user?.username !== userNameInput ? '#3E629F' : 'rgba(0,0,0,0.2)', borderRadius: 8, cursor: 'pointer'}}>
-                <p style={{color: 'white', fontWeight: 'bold', fontSize: 15}}>UPDATE</p>
+                <p style={{color: 'white', fontWeight: 'bold', fontSize: 14}}>UPDATE</p>
+            </div>
+            <div style={{position: 'absolute', bottom: '0%', width: '100%', height: '25%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                <div style={{width: '86%', height: '40%', backgroundColor: 'green', borderRadius: 8, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <p style={{color: 'white', fontWeight: 'bold', fontSize: 14}}>LOG OUT</p>
+                </div>
+                <div>
+                    <p style={{color: '#ff0628', fontWeight: 'bold', fontSize: 14}}>DELETE ACCOUNT</p>
+                </div>
             </div>
         </div>
     )
