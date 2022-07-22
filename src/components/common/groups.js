460610394ca97cd117f5f483c4efaf39b1d48039
Aftab Sidhu <Aftab180@gmail.com>
2022-07-22 13:44:38 -0400

    update

--- src/components/common/groups.js
+++ src/components/common/groups.js
@@ -6,6 +6,7 @@ const Groups = (props) => {
 
     const { select } = props;
     const dispatch = useDispatch();
+    const user = useSelector(state => state.user);
     const chats = useSelector(state => state.chat);
 
     const selectDefaultChat = async () => {
@@ -17,9 +18,14 @@ const Groups = (props) => {
 
     return (
         <div style={{height: '100%', width: '100%', display: 'flex', alignItems: 'center', overflowY: 'scroll', flexDirection: 'column'}}>
-            {chats.map((chat, index) => {
+            {chats.length > 0 ? chats.map((chat, index) => {
                 return <ChatCell key={index} chat={chat} index={index} select={select} />
-            })}
+            }) : (
+                <div style={{height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                    <p style={{color: 'rgba(255,255,255,0.8)', width: '90%', textAlign: 'center', fontSize: 18}}>Hey <strong>{user.username}</strong>!</p>
+                    <p style={{color: 'rgba(255,255,255,0.5)', width: '90%', textAlign: 'center', fontSize: 13, marginTop: '-2%'}}>To start conversation, please select friend and start chatting!</p>
+                </div>
+            )}
         </div>
     )
 }
